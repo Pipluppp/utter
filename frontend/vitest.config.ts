c4d40d9ad83a982637d6115d5b07704c3361264b
Duncan F. Bandojo <duncanb013@gmail.com>
2026-03-25 05:57:05 +0800

    chore: vitest passWithNoTests

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
     environment: "jsdom",
     include: ["src/__tests__/**/*.test.{ts,tsx}"],
     setupFiles: ["src/__tests__/setup.ts"],
+    passWithNoTests: true,
   },
 });
