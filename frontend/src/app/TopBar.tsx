6c8c050ee0c1455bfbafc3d77320df8045768773
Duncan F. Bandojo <duncanb013@gmail.com>
2026-03-29 04:14:38 +0800

    fix(TopBar): hide kbd shortcut indicators in mobile nav

--- frontend/src/app/TopBar.tsx
+++ frontend/src/app/TopBar.tsx
@@ -204,7 +204,6 @@ function MobileNavItem({
     >
       <NavItemContent
         label={item.label}
-        shortcut={item.shortcut}
         showTaskBadge={item.showTaskBadge}
         showProfileIcon={item.showProfileIcon}
       />
